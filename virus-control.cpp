3ddd578ef2ae8e7ffcb23f162391134f0dbebddd
mathababhassan <103527892+mathababhassan@users.noreply.github.com>
2023-04-08 21:56:12 +0200

    Create virus-control.cpp

---
+++ virus-control.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include<string>
+
+using namespace std;
+
+string removeSubString(string str, const string& substr) {
+    size_t pos = 0;
+    while ((pos = str.find(substr, pos)) != string::npos)
+    {// the while loop makes sure all the occurences of the substring are removed
+        str.replace(pos, substr.length(), ""); // '' indicates that we don't replace the string with anything, we just remove it 
+    }
+    return str;
+}
+
+
+int main() {
+    string s;
+    cin >> s;
+    cin.ignore();
+    size_t originalLenOfStr = s.length();
+    int daysCount;
+    
+    for(int i = 0; i < s.length(); ++i)
+    {
+        removeSubString(s, "LR");
+        removeSubstring(s, '#');
+        if(s.length() < originalLenOfStr)
+            ++daysCount;
+    }
+    return 0;
+}
